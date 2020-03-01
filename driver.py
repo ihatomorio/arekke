d0a272ebe8cd9a9807ed0469ae49b3502738e630
you <you@example.com>
2020-03-01 15:57:16 +0900

    テストドライバーを切り出し

---
+++ driver.py
@@ -0,0 +1,11 @@
+import get_single_items
+
+
+url = 'https://booth.pm/ja/items/813108' #マステ
+# url = 'https://booth.pm/ja/items/1759716'
+# url = 'https://www.dmm.co.jp/dc/doujin/-/detail/=/cid=d_aoi0538/'
+# url = 'https://www.dlsite.com/maniax/work/=/product_id/RJ278847.html'
+# url = 'https://www.melonbooks.co.jp/detail/detail.php?product_id=633353
+# url = 'https://book.dmm.co.jp/detail/b158aakn00788/' #COMIC LO 2019年11月号
+
+get_single_items.get_single_item(url)
