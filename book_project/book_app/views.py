207823894bd1d3857f6012b18a5605dc4ea6a3ce
you <you@example.com>
2020-03-04 23:04:09 +0900

    viewを追加

--- book_project/book_app/views.py
+++ book_project/book_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+from book_app.models import Book
+
+class BookListView(TemplateView):
+    template_name = "book_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        return render(self.request, self.template_name, context)
