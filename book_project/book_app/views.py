5b9d729af1596f4bf6a072fb01ab4dd03ce09877
you <you@example.com>
2020-03-07 18:35:00 +0900

    url参照を変更

--- book_project/book_app/views.py
+++ book_project/book_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
 from django.views.generic import TemplateView
+from django.utils import timezone
+from . import models
 
-from book_app.models import Book
+def book_list(request):
+    books = models.Book.objects.all()
+    return render(request, 'book_app/book_list.html', {'books': books})
+
 
 class BookListView(TemplateView):
     template_name = "book_list.html"
@@ -10,7 +14,7 @@ class BookListView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = super(BookListView, self).get_context_data(**kwargs)
 
-        book_objs = Book.objects.all()
+        book_objs = models.Book.objects.all()
         context['books'] = book_objs
 
         return render(self.request, self.template_name, context)
