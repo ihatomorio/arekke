0a4851c376f1ad807d85fd06c0d6b47ca5235523
you <you@example.com>
2020-03-08 04:01:55 +0900

    Fix importの方法を統一

--- book_project/book_app/views.py
+++ book_project/book_app/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.utils import timezone
-from django.db import models
-import book_app.models
-import book_app.forms
+from .models import Book, Product
+from .forms import BookForm, ProductForm
 from engine import webscraper
 
 def book_list(request):
-    books = book_app.models.Book.objects.all()
+    books = Book.objects.all()
     return render(request, 'book_app/book_list.html', {'books': books})
 
 
 def book_new(request):
     if request.method == "POST":
-        form = book_app.forms.BookForm(request.POST)
+        form = BookForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.owner = request.user
@@ -21,33 +20,35 @@ def book_new(request):
 
             return redirect('/book/list/', pk=post.pk)
     else:
-        form = book_app.forms.BookForm()
+        form = BookForm()
     return render(request, 'book_app/book_new.html', {'form': form})
 
 
 def product_list(request):
-    products = book_app.models.Product.objects.all()
+    products = Product.objects.all()
     if request.method == "POST":
         # POSTリクエストからpkを取り出す
         pk_id = request.POST.get('pk',None)
 
         # 指定のpkからobjを取り出す
-        obj = book_app.models.Product.objects.get(pk=pk_id)
+        obj = Product.objects.get(pk=pk_id)
 
         # Webスクレイピングを実行
         webscraper.get_single_item(obj)
 
-        # objを取得してきた情報で更新
+        # objを取得してきた情報で確定
         obj.save()
         obj.info.save()
-    else:
-        print("hoge")
+
+        # 同じページにリダイレクトしてPOSTの要求をクリアする
+        return redirect('/product/list/')
+    
     return render(request, 'book_app/product_list.html', {'products': products})
 
 
 def product_new(request):
     if request.method == "POST":
-        form = book_app.forms.ProductForm(request.POST, request.FILES)
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.owner = request.user
@@ -57,6 +58,6 @@ def product_new(request):
 
             return redirect('/product/list/', pk=post.pk)
     else:
-        form = book_app.forms.ProductForm()
+        form = ProductForm()
     return render(request, 'book_app/product_new.html', {'form': form})
 
