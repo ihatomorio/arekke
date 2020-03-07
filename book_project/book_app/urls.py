a4a4c326a5ebe6c5f4a5375d39cf076cc6170534
you <you@example.com>
2020-03-07 20:23:00 +0900

    フォームを追加

--- book_project/book_app/urls.py
+++ book_project/book_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
+    path('book/new/', views.book_new, name='book_new'),
 ]
--- book_project/book_app/views.py
+++ book_project/book_app/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.utils import timezone
 from . import models
+from . import forms
 
 def book_list(request):
     books = models.Book.objects.all()
     return render(request, 'book_app/book_list.html', {'books': books})
+
+
+def book_new(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+    else:
+        form = forms.BookForm()
+    return render(request, 'book_app/book_new.html', {'form': form})
 
 
 class BookListView(TemplateView):
