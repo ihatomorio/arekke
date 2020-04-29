173464274a19ad60bb8e9c0398b5828cf5805efe
mikankago <mikankago@hotmail.co.jp>
2020-04-30 04:21:48 +0900

    Add booth 商品ページが無い場合の処理

--- book_project/engine/shops/booth.py
+++ book_project/engine/shops/booth.py
@@ -22,6 +22,13 @@ class Booth(DoujinShop):
         return Product.BOOTH
 
     def _GetTitle(self):
+        # ページが無いことを検知する
+        try:
+            if 'The item you were looking for doesn\'t exist.' in self.driver.find_element_by_css_selector('h1').text:
+                return '商品ページなし'
+        except NoSuchElementException:
+            pass
+
         return self.driver.find_element_by_css_selector("h2.u-tpg-title1").text
 
     def _GetCircle(self):
